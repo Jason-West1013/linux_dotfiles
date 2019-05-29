93ee30b9cb8703a4921b7027e786664502990974
Jason-West1013 <jasonwest1013@yahoo.com>
2019-05-28 22:41:48 -0400

    Altered polybar config and i3

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,5 +9,6 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar -r  mainBar &
 polybar -r secondaryBar &
+polybar -r thirdBar &
 
 echo "Polybar launched..."
